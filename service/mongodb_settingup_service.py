13c29805d5da143be9c0443f466c63f38a473884
mirlayne <mirlayne.campuzano@gmail.com>
2021-10-20 21:10:00 -0400

    Using decorators.

--- service/mongodb_settingup_service.py
+++ service/mongodb_settingup_service.py
@@ -8,10 +8,12 @@ from entities.mongodb_collections_preprocess_interface import MongoDBCollections
 
 class MongoDBSettingUpService:
 
-    def __init__(self, mongodb_api_obj: IMongoAPI):
+    def __init__(self, mongodb_api_obj: IMongoAPI, mongodb_col_preproc: IMongoDBCollectionsPreprocess, json_folderpath: str):
         self.mongodb_api_obj = mongodb_api_obj
+        self.mongodb_col_preproc = mongodb_col_preproc
+        self.json_folderpath = json_folderpath
 
-    def mongo_export(self, mongodb_utils_filepath: str, hida_name: str, metadata_name: str, json_folderpath: str) -> None:
+    def mongo_export(self, mongodb_utils_filepath: str, hida_name: str, metadata_name: str) -> None:
         '''
         Function corresponding to "mongoExport" on file export2mongodb.py
         :param mongodb_utils_filepath: json file that stores the necessary data such as column names and utils json file names
@@ -38,84 +40,42 @@ class MongoDBSettingUpService:
         self.mongodb_api_obj.collection = hidaname
         hida_col_dict = self.mongodb_api_obj.read()
 
-        mongodb_api_obj = IMongoAPI()
-
         if mongodb_column_dict.get("ispattern", False):
-            json_path = os.path.join(json_folderpath, mongodb_files_dict["pattern"])
-            mongodb_col_preproc.load_array_collection(json_path)
-            mongodb_api_obj.collection = "pattern"
-            mongodb_api_obj.insert_in_collection(data)
+            self.create_fill_collection(mongodb_files_dict["pattern"])
 
         if mongodb_column_dict.get("isfolders", False):
-            json_path = os.path.join(json_folderpath, mongodb_files_dict["files"])
-            data = mongodb_col_preproc.load_array_collection(json_path)
-            mongodb_api_obj.collection = "folders"
-            mongodb_api_obj.insert_in_collection(data)
-
-            json_path = os.path.join(json_folderpath, mongodb_files_dict["koepnick_files"])
-            data = mongodb_col_preproc.load_array_collection(json_path)
-            mongodb_api_obj.collection = "koepnick_folders"
-            mongodb_api_obj.insert_in_collection(data)
+            self.create_fill_collection(mongodb_files_dict["files"])
+            self.create_fill_collection(mongodb_files_dict["koepnick_files"])
 
         if mongodb_column_dict.get("isbadlist", False):
-            json_path = os.path.join(json_folderpath, mongodb_files_dict["badlist"])
-            data = mongodb_col_preproc.load_array_collection(json_path)
-            mongodb_api_obj.collection = "badlist"
-            mongodb_api_obj.insert_in_collection(data)
+            self.create_fill_collection(mongodb_files_dict["badlist"])
 
         if mongodb_column_dict.get("isvorhaben", False):
-            json_path = os.path.join(json_folderpath, mongodb_files_dict["vorhaben"])
-            data = mongodb_col_preproc.load_array_collection(json_path)
-            mongodb_api_obj.collection = "vorhaben"
-            mongodb_api_obj.insert_in_collection(data)
+            self.create_fill_collection(mongodb_files_dict["vorhaben"])
 
         if mongodb_column_dict.get("istopics", False):
-            json_path = os.path.join(json_folderpath, mongodb_files_dict["topics3a"])
-            data = mongodb_col_preproc.load_array_collection(json_path)
-            mongodb_api_obj.collection = "topics"
-            mongodb_api_obj.insert_in_collection(data)
+            self.create_fill_collection(mongodb_files_dict["topics3a"])
 
         if mongodb_column_dict.get("isvorhabeninv", False):
-            json_path = os.path.join(json_folderpath, mongodb_files_dict["vorhaben_inv"])
-            data = mongodb_col_preproc.load_array_collection(json_path)
-            mongodb_api_obj.collection = "vorhaben_inv"
-            mongodb_api_obj.insert_in_collection(data)
+            self.create_fill_collection(mongodb_files_dict["vorhaben_inv"])
 
         if mongodb_column_dict.get("istaxo", False):
-            json_path = os.path.join(json_folderpath, mongodb_files_dict["taxo"])
-            data = mongodb_col_preproc.load_array_collection(json_path)
-            mongodb_api_obj.collection = "taxo"
-            mongodb_api_obj.insert_in_collection(data)
+            self.create_fill_collection(mongodb_files_dict["taxo"])
 
         if mongodb_column_dict.get("isinvtaxo", False):
-            json_path = os.path.join(json_folderpath, mongodb_files_dict["taxo_inv"])
-            data = mongodb_col_preproc.load_array_collection(json_path)
-            mongodb_api_obj.collection = "invtaxo"
-            mongodb_api_obj.insert_in_collection(data)
+            self.create_fill_collection(mongodb_files_dict["taxo_inv"])
 
         if mongodb_column_dict.get("isemblist", False):
-            json_path = os.path.join(json_folderpath, mongodb_files_dict["all_matches"])
-            data = mongodb_col_preproc.load_embeddings(json_path)
-            mongodb_api_obj.collection = "emblist"
-            mongodb_api_obj.insert_in_collection(data)
+            self.create_fill_collection(mongodb_files_dict["all_matches"])
 
         if mongodb_column_dict.get("isnoemblist", False):
-            json_path = os.path.join(json_folderpath, mongodb_files_dict["no_matches"])
-            data = mongodb_col_preproc.load_no_matches(json_path)
-            mongodb_api_obj.collection = "noemblist"
-            mongodb_api_obj.insert_in_collection(data)
+            self.create_fill_collection(mongodb_files_dict["no_matches"])
 
         if mongodb_column_dict.get("ishida", False):
-            json_path = os.path.join(json_folderpath, mongodb_files_dict["hida"])
-            data = mongodb_col_preproc.patch_hida(json_path)
-            mongodb_api_obj.collection = hidaname
-            mongodb_api_obj.insert_in_collection(data)
+            self.create_fill_collection(mongodb_files_dict["hida"])
 
         if mongodb_column_dict.get("isresolved", False):
-            json_path = os.path.join(json_folderpath, mongodb_files_dict["resolved"])
-            data = mongodb_col_preproc.patch_resolved(json_path, hida_col_dict)
-            mongodb_api_obj.collection = metadataname
-            mongodb_api_obj.insert_in_collection(data)
+            self.create_fill_collection(mongodb_files_dict["resolved"])
 
         if mongodb_column_dict.get("iscategories", False) or mongodb_column_dict.get("isvorhabeninv", False):
             json_path = os.path.join(json_folderpath, mongodb_files_dict["vorhaben_inv"])
@@ -124,14 +84,27 @@ class MongoDBSettingUpService:
             mongodb_api_obj.write(data)
 
         if mongodb_column_dict.get("ismetadatahida", False):
-            data_gen = mongodb_col_preproc.project_metadata_hida(metadata_col_obj, hida_col_dict)
-            mongodb_api_obj.collection = metadataname
-            while True:
-                try:
-                    data = next(data_gen)
-                    mongodb_api_obj.update(data)
-                except StopIteration:
-                    break
+            self.create_insert_one(self.mongodb_col_preproc.project_metadata_hida, metadataname, metadata_col_obj, hida_col_dict)
+
+        if mongodb_column_dict.get("ismetadatanokeywords", False):
+            self.create_insert_one(self.mongodb_col_preproc.unproject_metadata_keywords, metadataname, metadata_col_obj)
+
+        if mongodb_column_dict.get("isresolved", False) or mongodb_column_dict.get("isupdatehida", False):
+            self.create_insert_one(self.mongodb_col_preproc.project_hida, metadataname, metadata_col_obj)
+
+        if mongodb_column_dict.get("isresolved", False) or mongodb_column_dict.get("isupdatevorhaben", False):
+            self.create_insert_one(self.mongodb_col_preproc.patch_vorhaben, metadataname, metadata_col_obj)
+
+        if mongodb_column_dict.get("isresolved", False) or mongodb_column_dict.get("isupdatetaxo", False) or \
+                mongodb_column_dict.get("ismetadatahida", False):
+            self.mongodb_api_obj.collection = "invtaxo"
+            invtaxo_list = self.mongodb_api_obj.read()
+            self.create_insert_one(self.mongodb_col_preproc.project_metadata_hida, metadataname, metadata_col_obj, invtaxo_list)
+
+        if mongodb_column_dict.get("ishida", False) or mongodb_column_dict.get("isupdatehidataxo", False):
+            self.mongodb_api_obj.collection = "invtaxo"
+            invtaxo_list = self.mongodb_api_obj.read()
+            self.create_insert_one(self.mongodb_col_preproc.project_metadata_hida, hidaname, metadata_col_obj, invtaxo_list)
 
         if mongodb_column_dict.get("ispatch_dir", False) or mongodb_column_dict.get("isresolved", False):
             mongodb_api_obj.collection = "folders"
@@ -166,67 +139,57 @@ class MongoDBSettingUpService:
                 except StopIteration:
                     break
 
-        if mongodb_column_dict.get("ismetadatanokeywords", False):
-            data_gen = mongodb_col_preproc.unproject_metadata_keywords(metadata_col_obj)
-            mongodb_api_obj.collection = metadataname
-            while True:
-                try:
-                    data = next(data_gen)
-                    mongodb_api_obj.update(data)
-                except StopIteration:
-                    break
-
-        if mongodb_column_dict.get("isresolved", False) or mongodb_column_dict.get("isupdatehida", False):
-            data_gen = mongodb_col_preproc.project_hida(metadata_col_obj)
-            mongodb_api_obj.collection = metadataname
-            while True:
-                try:
-                    data = next(data_gen)
-                    mongodb_api_obj.update(data)
-                except StopIteration:
-                    break
-
-        if mongodb_column_dict.get("isresolved", False) or mongodb_column_dict.get("isupdatevorhaben", False):
-            data_gen = mongodb_col_preproc.patch_vorhaben(metadata_col_obj)
-            mongodb_api_obj.collection = metadataname
-            while True:
-                try:
-                    data = next(data_gen)
-                    mongodb_api_obj.update(data)
-                except StopIteration:
-                    break
-
-        if mongodb_column_dict.get("isresolved", False) or mongodb_column_dict.get("isupdatetaxo", False) or \
-                mongodb_column_dict.get("ismetadatahida", False):
-            mongodb_api_obj.collection = "invtaxo"
-            invtaxo_list = mongodb_api_obj.read()
-            data_gen = mongodb_col_preproc.patch_inv_taxo(metadata_col_obj, invtaxo_list)
-            mongodb_api_obj.collection = metadataname
-            while True:
-                try:
-                    data = next(data_gen)
-                    mongodb_api_obj.update(data)
-                except StopIteration:
-                    break
-
-        if mongodb_column_dict.get("ishida", False) or mongodb_column_dict.get("isupdatehidataxo", False):
-            mongodb_api_obj.collection = "invtaxo"
-            invtaxo_list = mongodb_api_obj.read()
-            data_gen = mongodb_col_preproc.project_hida_inv_taxo(hida_col_dict, invtaxo_list)
-            mongodb_api_obj.collection = hidaname
-            while True:
-                try:
-                    data = next(data_gen)
-                    mongodb_api_obj.update(data)
-                except StopIteration:
-                    break
-
-    def create_fill_collection(self, collection_name: str, collection_function: any, **function_args) -> None:
-        def wrapper():
-            data = collection_function(function_args)
-            self.mongodb_api_obj.collection = collection_name
+    def create_fill_collection(self, collection_function: any) -> None:
+        def wrapper(*args, **kwargs):
+            data = collection_function(*args, **kwargs)
+            self.mongodb_api_obj.collection = args[0]  # collection_name: str
             self.mongodb_api_obj.insert_in_collection(data)
         return wrapper
+
+    @create_fill_collection
+    def collection_from_json(self, collection_name: str):
+        json_path = os.path.join(self.json_folderpath, collection_name)
+        return self.mongodb_col_preproc.load_array_collection(json_path)
+
+    def insert_one_in_collection(self, collection_function: any) -> None:
+        def wrapper(*args, **kwargs):
+            data_gen = collection_function(*args, **kwargs)
+            self.mongodb_api_obj.collection = args[0]  # collection_name: str
+            while True:
+                try:
+                    data = next(data_gen)
+                    self.mongodb_api_obj.write(data)
+                except StopIteration:
+                    break
+        return wrapper
+
+    @insert_one_in_collection
+    def create_insert_one(self, **kwargs):
+        collection_name = kwargs["collection_name"]
+        function_ref = kwargs["function_ref"]
+        function_args = kwargs["function_args"]
+        json_path = os.path.join(self.json_folderpath, collection_name)
+        return function_ref(*function_args, json_path)
+
+    def insert_many_in_collection(self, collection_function: any) -> None:
+        def wrapper(*args, **kwargs):
+            data_gen = collection_function(*args, **kwargs)
+            self.mongodb_api_obj.collection = args[0]  # collection_name: str
+            while True:
+                try:
+                    data = next(data_gen)
+                    self.mongodb_api_obj.write(data)
+                except StopIteration:
+                    break
+        return wrapper
+
+    @insert_many_in_collection
+    def create_insert_many(self, **kwargs):
+        collection_name = kwargs["collection_name"]
+        function_ref = kwargs["function_ref"]
+        function_args = kwargs["function_args"]
+        json_path = os.path.join(self.json_folderpath, collection_name)
+        return function_ref(*function_args, json_path)
 
     def extract_metadata(self, mongodb_utils_filepath: str, hida_name: str, metadata_name: str, json_folderpath: str) -> None:
         metadataname = metadata_name
